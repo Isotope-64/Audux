8197bc27ab3fe95d5177769e90697f15ef75175d
Crayzato <67732822+Crayzato@users.noreply.github.com>
2023-08-29 02:06:37 +1000

    begun voice recognition
    the software can now interpret audio files as strings - need to check with team about how to import these audio files into the code

--- audux.py
+++ audux.py
@@ -5,6 +5,11 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from nltk import RegexpParser, tokenize, pos_tag, stem as _l
 l = _l.WordNetLemmatizer()
 c = RegexpParser("NP: {<DT>?<JJ>*<NN>}")
+import speech_recognition as sr
+r = sr.Recognizer()
+
+temp_voice_command = sr.AudioFile('say hello.wav')
+with temp_voice_command as source: audio = r.record(source)
 
 files = []
 for (dir_path, dir_names, file_names) in walk("__commands"): files.extend([f[:-3] for f in file_names if f[-3:] == ".py"])
@@ -12,9 +17,9 @@ for (dir_path, dir_names, file_names) in walk("__commands"): files.extend([f[:-3
 #       Making a class for each word. When the word is initialised, it automatically gets processed.
 #This allows for in future creating functions that affect the words
 class Word():
-    def __init__(self, string, type):
-        self.original_text = string # --> just in case we ever need to access this
-        self.val = l.lemmatize(string) # --> the root of the word. This is typically the useful bit. E.g cases->case worst->bad
+    def __init__(self, string, type = -1):
+        self.val = string # --> just in case we ever need to access this
+        #self.val = l.lemmatize(string) # --> the root of the word. This is typically the useful bit. E.g cases->case worst->bad
         self.type = type # --> the part of speech. E.g adjective, noun
     def __str__(self) -> str: return str(self.val)
 
@@ -24,10 +29,12 @@ def interpret_command(cmd : Word, args : list = []):
         command_file.fnc(args)
 
 while (cmd := input("<:: ")) != "":
+    if cmd == "voice": cmd = r.recognize_google(audio)
+    
     # loops infintely, assigning cmd to the user input. If the user inputs nothing, the loop breaks
     words = tokenize.word_tokenize(cmd) # --> separates the string into words
     words = pos_tag(words)
-    words = c.parse(words)
-    interpret_command(Word(*words[0][0]))
+    words_tree  = c.parse(words)
+    interpret_command(Word(*words[0]), [Word(*i) for i in words][1:])
 
 
